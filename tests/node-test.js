a39bdd82decae08cbd4380c60cc241f16c7dab97
Emily Kuckelman <ekuckelman@gmail.com>
2018-02-12 18:44:37 -0700

    Working on suggest method still

---
+++ tests/node-test.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import Node from '../lib/Node';
+
+describe('NODE', () => {
+  let node;
+
+  beforeEach(() => {
+    node = new Node('pizza')
+  })
+
+  it('should exist', () => {
+    expect(node).to.exist;
+  })
+
+  it('should be able to store child nodes', () => {
+
+  });
+
+})
