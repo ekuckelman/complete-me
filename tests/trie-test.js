6189615b4f86a33dd916f515d0ca64bb069e06fb
Emily Kuckelman <ekuckelman@gmail.com>
2018-02-13 18:46:34 -0700

    Delete method completed

--- tests/trie-test.js
+++ tests/trie-test.js
@@ -2,6 +2,11 @@ import { expect } from 'chai';
 import Node from '../lib/Node';
 import Trie from '../lib/Trie';
 
+import fs from 'fs';
+
+const text = "/usr/share/dict/words"
+const dictionary = fs.readFileSync(text).toString().trim().split('\n')
+
 
 describe('Trie', () => {
   let trie;
@@ -47,13 +52,70 @@ describe('Trie', () => {
     });
   });
 
-  describe('suggest', () => {
-    it('should return an array of suggested words', () => {
-      let results = trie.suggest('pi');
-      let check1 = results.some(result => result === 'pizza');
-      let check2 = results.some(result => result === 'pizzas');
-      let check3 = results.some(result => result === 'piano');
-      let check4 = results.some(result => result === 'dog');
+  describe('Suggest', () => {
+    it('should suggest a word based on a prefix', () => {
+      trie.insert('doggo');
+      let suggestions = trie.suggest('do');
+      expect(suggestions).to.eql(['doggo'])
+    })
+    it('should suggest multiple words', () => {
+      trie.insert('doggo');
+      trie.insert('dog');
+      trie.insert('doggy');
+      trie.insert('piano');
+      trie.insert('pizza');
+      trie.insert('doggoneprefixtries');
+      trie.insert('piazza');
+      trie.insert('tomato');
+      let suggestions = trie.suggest('do');
+      expect(suggestions.some(current => current === 'doggo')).to.be.true
+      expect(suggestions.some(current => current === 'dog')).to.be.true
+      expect(suggestions.some(current => current === 'doggy')).to.be.true
+      expect(suggestions.some(current => current === 'doggoneprefixtries')).to.be.true 
+      let options = trie.suggest('pi');
+      expect(options.some(current => current === 'pizza')).to.be.true
+      expect(options.some(current => current === 'piazza')).to.be.true
+      expect(options.some(current => current === 'piano')).to.be.true
+      let tWord = trie.suggest('to');
+      expect(tWord.some(current => current === 'tomato')).to.be.true
+    });
+  });
+  describe('Populate', () => {
+    it('should Populate a dictionary', () => {
+      expect(trie.count).to.equal(0);
+
+      trie.populate(dictionary);
+
+      expect(trie.count).to.equal(235886);
+    });
+
+    it('should suggest words from the dictionary', () => {
+      trie.populate(dictionary);
+
+      expect(trie.suggest('piz')).to.deep.equal([ 'pize', 'pizza', 'pizzeria', 'pizzicato', 'pizzle' ]);
+    });
+  });
+  describe('select', () => {
+    it('should prioritize ', () => {
+      trie.populate(dictionary);
+
+      expect(trie.suggest('piz')).to.deep.equal([ 'pize', 'pizza', 'pizzeria', 'pizzicato', 'pizzle' ]);
+      trie.select('pizzeria');
+
+      expect(trie.suggest('piz')).to.deep.equal([ 'pizzeria', 'pize', 'pizza', 'pizzicato', 'pizzle' ]);
+    });
+  });
+  describe('delete', () => {
+    it('should delete the word ', () => {
+      trie.populate(dictionary);
+
+
+      expect(trie.suggest('piz')).to.deep.equal([ 'pize', 'pizza', 'pizzeria', 'pizzicato', 'pizzle' ]);
+      
+      trie.delete('pizzeria');
+
+
+      expect(trie.suggest('piz')).to.deep.equal([ 'pize', 'pizza', 'pizzicato', 'pizzle' ]);
     });
   });
 });
